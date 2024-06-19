32a0fae73b3bdab6eb6ebdbf5427a1133c2c8d3d
Madhavhk04 <148519558+Madhavhk04@users.noreply.github.com>
2024-06-19 17:37:34 +0400

    madhavvvvvvv

--- backend.py
+++ backend.py
@@ -8,21 +8,32 @@ config = {
     'database': 'futurense'
 }
 
+
+
 def connect_to_database(config):
     try:
         connection = mysql.connector.connect(**config)
         if connection.is_connected():
+
             print("Connection successful")
             cursor = connection.cursor()
+
             return connection
     except mysql.connector.Error as err:
+
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
+
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
             print("Database does not exist")
+
+
         else:
+
             print(err)
     return None
+
+
 
 connection = connect_to_database(config)
 if connection:
